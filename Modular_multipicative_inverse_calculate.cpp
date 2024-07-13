bfbd693ddb849641bd1c868c87e983e129d29f3c
Atikur Rahman <54862457+atik-1010@users.noreply.github.com>
2024-07-13 20:34:41 +0600

    Number Theory

--- Modular_multipicative_inverse_calculate.cpp
+++ Modular_multipicative_inverse_calculate.cpp
@@ -1,24 +1,26 @@
 
+#define mod = 1000000007
+#define N = 1000006
 
 ///Modular m.Inverse calculate
 //log(n)
-ll powerMod(ll a, ll b, ll p){
-    if(b==0)return 1%p;
+ll powerMod(ll a, ll b){
+    if(b==0)return 1%mod;
 
     if(b%2==0){
-        ll x = powerMod(a, b/2, p);
-        return (x*x)%p;
+        ll x = powerMod(a, b/2);
+        return (x*x)%mod;
     }
     else{
-        ll x = powerMod(a, p/2, p);
-        return ((x*x)%p * a)%p;
+        ll x = powerMod(a, b/2);
+        return ((x*x)%mod * a)%mod;
     }
 
     return 0;
 }
 
 ll inverseMod(ll a, ll b){
-    return powerMod(a, p-2, p);
+    return powerMod(a, mod-2);
 }
 
 
