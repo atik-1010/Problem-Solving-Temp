b7fcb820fe7709d170a2b64799d7c804bd5aeb62
Atikur Rahman <54862457+atik-1010@users.noreply.github.com>
2025-11-01 19:01:33 +0600

    Partial Sum

---
+++ partial_prefix_sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// akta array dewa ache, and k dewa ache,  and kichu range dewa ache protita range ar sathe k jog last array ta output dite hobe
+void fun(){
+  int n;
+  cin>>n;
+  int q;
+  cin>>q;
+  std::vector<int>pre(n+2);
+  while(q--){
+    int l, r;
+    cin>>l>>r;
+    
+    pre[l]++;
+    pre[r+1]--;
+    
+  }
+  for(int i=1; i<=n; i++){
+    pre[i] +=pre[i-1];
+  }
+  for(int i=1; i<=n; i++){
+    cout<<pre[i]<<" \n"[i==n];
+  }
+}
+int main() 
+{
+    fun();
+    return 0;
+}
